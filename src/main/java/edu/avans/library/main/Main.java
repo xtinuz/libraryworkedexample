8222cafc21f6d141ae47136a36f55c92aea96855
xtinuz <fanat0@hotmail.com>
2015-05-21 10:22:10 +0200

    Update Main.java

--- src/main/java/edu/avans/library/main/Main.java
+++ src/main/java/edu/avans/library/main/Main.java
@@ -27,6 +27,6 @@ public class Main {
     public static void main(String[] args) {
 
         MemberAdminUI ui = new MemberAdminUI(new MemberAdminManagerImpl());
-        ui.setVisible(true);
+        ui.setVisible(false);
     }
 }
